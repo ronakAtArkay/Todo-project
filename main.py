a9cacd4b7516d466404bfc602b3adf1fc5de0933
ronak <ronak@arkayapps.com>
2022-11-26 16:52:56 +0530

    return user's all detail

--- main.py
+++ main.py
@@ -17,48 +17,48 @@ def get_db():
 
 
 # create a user
-@app.post("/users/")
+@app.post("/todo/users/")
 def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
     return crud.create_user(db = db, user=user)
 
 # search user by name
-@app.get("/users/{user_name}", response_model=schemas.UserBase)
-def read_user(user_name = str,  db : Session = Depends(get_db)):
+@app.get("/todo/users/{user_name}/")
+def get_user_by_name(user_name : str, db : Session = Depends(get_db)):
     users = crud.get_name(db, user_name = user_name)
     if users is None:
         raise HTTPException(status_code= 404, detail="User not Found")
     return users
 
 # get all users data
-@app.get("/users/", response_model= List[schemas.UserBase])
-def read_users(skip : int = 0, limit : int = 100, db: Session = Depends(get_db)):
+@app.get("/todo/users/")
+def get_users_all_data(skip : int = 0, limit : int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit= limit)
     return users
 
 
 # search user by id
-@app.get("/users/{user_id}/")
-def all_user(user_id, db: Session = Depends(get_db)):
+@app.get("/todo/users/{user_id}")
+def get_user_by_id(user_id, db: Session = Depends(get_db)):
     users = crud.get_names(db, user_id)
     if users is None:
         raise HTTPException(status_code= 404, detail="User not Found")
     return users
 
 # update a user by id
-@app.put("/users/{id}")
-def update(user_name : schemas.UserBase, id : str,  db: Session = Depends(get_db)):
+@app.put("/todo/users/{id}")
+def update_user(user_name : schemas.UserBase, id : str,  db: Session = Depends(get_db)):
     users =  crud.update(db, user_name = user_name, id=id)
     return users
 
 # delete a user by id
-@app.delete("/users/{id}")
-def delete_user(id : str, db: Session = Depends(get_db)):
+@app.delete("/todo/users/{id}")
+def delete_user_data(id : str, db: Session = Depends(get_db)):
     users = crud.delete_user(db, id=id)
     return users
 
 # status change by delete
-@app.delete("/users/{id}/")
-def status_changed(id : str, db: Session = Depends(get_db)):
+@app.delete("/todo/users/{id}/")
+def delete_user_without_data(id : str, db: Session = Depends(get_db)):
     users = crud.status_change(db, id = id)
     return users
 
